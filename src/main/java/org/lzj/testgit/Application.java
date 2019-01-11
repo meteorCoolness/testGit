7d8ab5db38db132718cc556ae2dafbbadc363a11
liu.zj <liu.zj@qq.com>
2019-01-11 11:13:37 +0800

    test branch

--- src/main/java/org/lzj/testgit/Application.java
+++ src/main/java/org/lzj/testgit/Application.java
@@ -4,4 +4,10 @@ public class Application {
 	public static void main(String[] args) {
 		System.out.println("test...");
 	}
+
+	public Application() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+	
 }
